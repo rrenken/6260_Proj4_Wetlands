a21d575b8ecdb4ed0cf5cfa09aae8b09c49e8d5d
Sameer-khan-786 <85848440+Sameer-khan-786@users.noreply.github.com>
2023-12-02 18:39:54 -0500

    Update script.js

--- Scatterplotsoil/script.js
+++ Scatterplotsoil/script.js
@@ -81,7 +81,7 @@ d3.json('modified_soil_data.json').then(function(data)
 
     
     let margin = {top: 30, right: 20, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
+        width = 700 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
     
